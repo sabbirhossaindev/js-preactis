8ed94932e3064daed16631a4d408dc901678cbda
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-02 23:06:46 +0600

    5 problem solve

--- variable.js
+++ variable.js
@@ -15,3 +15,20 @@ VARIABLE
 11. parseInt, ParseFloat
 12. toFixed
 */
+
+// 1. What is JavaScript?
+// => javscript is a high lavel, interpreated programming language use to make web page more Intersection.
+
+// 2. How does JS Works ?
+// => web design, mobile apps, stop watch, game, apps, flying robat, web server, web application etc.
+
+// 3. What is Variable ?
+// => In programming, a variable is a value that can change, depending on conditions or on information passed to the program
+
+// 4. Declare a variable
+var declare = "variable";
+console.log(declare);
+
+// 5. Types of Variable? How can you find out type of a variable
+var chackType = "variable";
+console.log(typeof chackType);
