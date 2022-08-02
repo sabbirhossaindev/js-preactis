b7082dcca060aeea1d398cc0d26af1909942b2ee
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-03 00:02:45 +0600

    7 problem solve

--- variable.js
+++ variable.js
@@ -32,3 +32,52 @@ console.log(declare);
 // 5. Types of Variable? How can you find out type of a variable
 var chackType = "variable";
 console.log(typeof chackType);
+
+//6. Primitive and non-primitive data types
+// Primitive Explore
+//variable
+var string = "sabbir";
+//string
+var number = 100;
+// undefined
+var a;
+console.log(a);
+// null
+var nam = null;
+console.log(nam);
+// Boolen
+var bool = true;
+console.log(typeof bool)
+var bools = false;
+console.log(typeof bools)
+
+//non-primitive data types
+// 1. object
+// 2. array
+// 3. funtion
+
+// 7. Naming Convention of JS variables
+var myConvention = 100; // camelCase.
+
+// 8. Math Operation +, -, *, /, %
+var a = 10;
+var b = 20;
+console.log(a + b);
+
+var a = 10;
+var b = 20;
+console.log(a - b);
+
+var a = 10;
+var b = 20;
+console.log(a * b);
+
+var a = 10;
+var b = 20;
+console.log(a / b);
+
+var a = 10;
+var b = 20;
+console.log(a % b);
+
+//9. Short hand: +=, -=, *=, /=
